2aeb27465cf6acccc41c465ebff2b26eb3f64b20
Kimmyauw <meribly159@gmail.com>
2026-09-11 12:29:05 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Special Just For Kimmy`.split('')
+const text = `Special Just For Dhea Sayang`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
